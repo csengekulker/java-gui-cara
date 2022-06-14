e603aac37277a8285aab5fea698df4ee3cb9d795
csengekulker <k71608883429@ktch.hu>
2022-06-14 08:59:35 +0200

    ú

--- src/views/Car.java
+++ src/views/Car.java
@@ -1,26 +1,29 @@
 package views;
 
 import java.awt.Color;
-
 import javax.swing.JButton;
 
 public class Car extends JButton{
-    Color color;
+
     Integer actSpeed;
     int maxSpeed;
     boolean speedVisible;
-    String name;
+
 
     public Car(Color color, int initSpeed, String name) {
-        this.color = color;
+
+        this.setBackground(color);
+        this.setText(name);
         this.actSpeed = initSpeed;
         this.maxSpeed = 20;
         this.speedVisible = true;
-        this.name = name;
+
     }
+
     public Integer getActSpeed() {
         return this.actSpeed;
     }
+
     public void go() {
         int x = this.getLocation().x;
         int y = this.getLocation().y;
--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -1,7 +1,7 @@
 package views;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,13 +19,15 @@ public class MainFrame extends JFrame {
     }
     private void setMainPanel() {
         this.mainPanel = new JPanel();
+        this.mainPanel.setBackground(Color.BLACK);
         this.mainPanel.add(this.trackPanel);
 
-        this.setLayout(null);
+        this.mainPanel.setLayout(null);
     }
     private void setMainFrame() {
-        this.add(this.mainPanel, BorderLayout.CENTER);
         this.setLayout(new BorderLayout());
+        this.add(this.mainPanel, BorderLayout.CENTER);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 400);
         this.setVisible(true);
